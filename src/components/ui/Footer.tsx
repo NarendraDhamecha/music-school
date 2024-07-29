48c3b698f05ebddaf32f7fd5262bb4df8a72354d
NarendraDhamecha <nikhildhamecha19@gmail.com>
2024-07-29 19:33:27 +0530

    added header and footer in layout

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <h1 className="text-center bg-slate-500 text-[18px]">Footer</h1>;
+}
+
+export default Footer;
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,5 @@
+function Header () {
+   return <h1 className="text-center bg-slate-500 text-[18px]">Header</h1>
+}
+
+export default Header
