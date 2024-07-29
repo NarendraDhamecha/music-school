a0698492cabce579a20bd8f69b562235b89909db
NarendraDhamecha <nikhildhamecha19@gmail.com>
2024-07-29 16:21:18 +0530

    nested dynamic route

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+function ReviewDetails({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) {
+  return (
+    <div>
+      Review {params.reviewId} for product {params.productId}
+    </div>
+  );
+}
+
+export default ReviewDetails;
---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,12 @@
+function Reviews() {
+  return (
+    <>
+      <h1>Review List</h1>
+      <div>Review 1</div>
+      <div>Review 3</div>
+      <div>Review 2</div>
+    </>
+  );
+}
+
+export default Reviews;
