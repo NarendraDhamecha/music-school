bfec267ea23fae5b5bdb1f8a4e8d4e7692387522
NarendraDhamecha <nikhildhamecha19@gmail.com>
2024-07-30 10:58:23 +0530

    routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About page",
+};
+
 function About() {
   return <h1>About Page</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,23 @@
-function ProductDetails({ params }: { params: { productId: string } }) {
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+function ProductDetails({ params }: Props) {
   return <div>Details about product {params.productId}</div>;
 }
 
